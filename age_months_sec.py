8e07ad3c594977f691de1ef4292e23b3a414b492
John <code25@tannerltd.com.com>
2018-03-19 21:57:03 -0500

    Adding age_months_sec.py file

---
+++ age_months_sec.py
@@ -0,0 +1,6 @@
+name = input('Enter your name: ')
+print(f'hello, {name}')
+age = int(input('Enter your age'))
+num_months = age * 12
+num_sec = age * 31536000
+print(f'you have lived for {num_months}  months or {num_sec} seconds.')
